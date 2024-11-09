6de2899c956c0efc51fde8a03156317a9c085a01
Liam Townsley <contact@liamtownsley.me>
2024-11-09 22:49:25 +0000

    fix: simplify camera file

--- Camera.py
+++ Camera.py
@@ -1,29 +1,14 @@
 import picamera # type: ignore
 from time import sleep
 
-camera = None
+camera = picamera.PiCamera()
 
 class Camera:
-    def __init__(self):
-        global camera
-        if camera is None:
-            camera = picamera.PiCamera()
-
-    def __del__(self):
-        global camera
-        camera = None
-
-    def start_recording(self, user_id:str, seconds:int):
-        if camera is None:
-            raise ValueError("Camera not initialised.")
-        
+    def start_recording(self, user_id:str, seconds:int):       
         camera.start_recording(f'{user_id}.h264')
         if seconds:
             sleep(seconds)
             self.end_recording()
     
     def end_recording(self):
-        if camera is None:
-            raise ValueError("Camera not initialised.")
-        
         camera.stop_recording()
