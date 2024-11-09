955bee37926bf2fbefb8347aa50fedc6074ed954
Liam Townsley <contact@liamtownsley.me>
2024-11-09 19:09:06 +0000

    fix: regex

--- Util.py
+++ Util.py
@@ -2,7 +2,7 @@ from RPi.GPIO import cleanup, setwarnings, setmode, BCM
 import re
 
 def clean_text(text):
-    filtered_text = re.sub(r'[^a-zA-Z0-9\s.,!?\-]', '', text)
+    filtered_text = re.sub(r'\W+', '', text)
     return filtered_text
 
 def initialise_gpio_pins():
