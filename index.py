41a146c1295ad4dc400fca8f1ebae72d0d9424b0
Liam Townsley <contact@liamtownsley.me>
2024-11-09 22:46:16 +0000

    fix

--- index.py
+++ index.py
@@ -85,7 +85,7 @@ def main_menu():
             menu[int(selection) - 1][1]() # Execute Menu function
         except Exception as e:
             print(f"Error in selection: {e.with_traceback(e.__traceback__)}\n")
-            pass
+            
 if __name__ == "__main__":
     try:
         while True:
