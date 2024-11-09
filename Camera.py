9ca2ca6d7809f1aa4b40dc1c5c3ada18c5e9f072
Liam Townsley <contact@liamtownsley.me>
2024-11-09 22:38:47 +0000

    feat: conditionally fill the global variable

--- Camera.py
+++ Camera.py
@@ -1,9 +1,17 @@
 import picamera # type: ignore
 from time import sleep
 
-camera = picamera.PiCamera()
+camera = None
 
-class Camera:  
+class Camera:
+    def __init__(self):
+        global camera
+        camera = picamera.PiCamera()
+
+    def __del__(self):
+        global camera
+        camera = None
+
     def start_recording(self, user_id:str, seconds:int):
         camera.start_recording(f'{user_id}.h264')
         if seconds:
