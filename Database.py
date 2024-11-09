9ccc29afcb45bf2783e1799e643216494d964632
Liam Townsley <contact@liamtownsley.me>
2024-11-09 19:47:59 +0000

    fix database

--- Database.py
+++ Database.py
@@ -1,12 +1,17 @@
 import pymongo
 
-client, database, collection = None, None, None
+client = None
+database = None
+collection = None
 
 def connect_to_database():
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    database = client["cmp408"]
-    collection = database["users"]
-    
+    try:
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        database = client["cmp408"]
+        collection = database["users"]
+    except Exception as e:
+        print(f"Exception occured whilst connecting to MongoDB: {e}")
+
 def get_user_by_card(card_id:str):
     user = collection.find_one({"card_id": card_id}, {})
     return user
