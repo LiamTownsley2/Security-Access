9b90cc7fdb7d51bd0b6c6508913791ae6e4ed816
Liam Townsley <contact@liamtownsley.me>
2024-11-09 22:41:12 +0000

    fix: 'NoneType' object is not subscriptable

--- Camera.py
+++ Camera.py
@@ -6,17 +6,24 @@ camera = None
 class Camera:
     def __init__(self):
         global camera
-        camera = picamera.PiCamera()
+        if camera is None:
+            camera = picamera.PiCamera()
 
     def __del__(self):
         global camera
         camera = None
 
     def start_recording(self, user_id:str, seconds:int):
+        if camera is None:
+            raise ValueError("Camera not initialised.")
+        
         camera.start_recording(f'{user_id}.h264')
         if seconds:
             sleep(seconds)
             self.end_recording()
     
     def end_recording(self):
+        if camera is None:
+            raise ValueError("Camera not initialised.")
+        
         camera.stop_recording()
