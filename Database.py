bc4c8e6f6bbb14c913aefcee5edf4c0747f07521
Liam Townsley <contact@liamtownsley.me>
2024-11-09 19:52:35 +0000

    fix: make variables global

--- Database.py
+++ Database.py
@@ -5,6 +5,7 @@ database = None
 collection = None
 
 def connect_to_database():
+    global client, database, collection
     try:
         client = pymongo.MongoClient("mongodb://2.tcp.eu.ngrok.io:13446/")
         database = client["cmp408"]
@@ -13,5 +14,7 @@ def connect_to_database():
         print(f"Exception occured whilst connecting to MongoDB: {e}")
 
 def get_user_by_card(card_id:str):
+    if not collection:
+        return False
     user = collection.find_one({"card_id": card_id}, {})
     return user
