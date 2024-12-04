62d11207bdb690ee8302dbe77b9a3b98252c4c7c
Liam Townsley <contact@liamtownsley.me>
2024-12-04 21:14:08 +0000

    fix: colour codes

--- index.py
+++ index.py
@@ -154,8 +154,8 @@ def main_menu(stdscr):
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-    curses.init_pair(5, curses.COLOR_RED, curses.COLOR_PAIRS)
-    curses.init_pair(6, curses.COLOR_GREEN, curses.COLOR_PAIRS)
+    curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(6, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
 
     
