2fa525cc863fe4d4ea0798a8fa5f2bd544423456
Liam Townsley <contact@liamtownsley.me>
2024-12-04 17:27:13 +0000

    fix: timezone() missing required argument 'offset'

--- AWS/DynamoDB.py
+++ AWS/DynamoDB.py
@@ -45,14 +45,14 @@ def register_entry(tag_id: str, user_id: Optional[str]):
         "LogID": str(generate_unique_id()),
         "TagID": tag_id,
         "UserID": user_id,
-        "Time": datetime.datetime.now(datetime.timezone().utc).isoformat()
+        "Time": datetime.datetime.now(datetime.timezone.utc).isoformat()
     }
     access_log_table.put_item(Item=entry)
 
     if user_id:
         user = get_user(user_id)
         if user:
-            user['LastScanned'] = datetime.datetime.now(datetime.timezone().utc).isoformat()
+            user['LastScanned'] = datetime.datetime.now(datetime.timezone.utc).isoformat()
             users_table.put_item(Item=user)
 
 def get_user(user_id: str):
