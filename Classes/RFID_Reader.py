b6763955896f12ce3596e8f13d6a43e920f7f350
Liam Townsley <contact@liamtownsley.me>
2024-12-04 18:36:32 +0000

    fix

--- Classes/RFID_Reader.py
+++ Classes/RFID_Reader.py
@@ -3,7 +3,6 @@ sys.path.append("..")
 
 from mfrc522 import SimpleMFRC522
 import Util
-from Encryptor import decrypt_message, encrypt_message
 
 class RFID_Reader:
     def __init__(self):
@@ -14,8 +13,3 @@ class RFID_Reader:
         id, text = self.reader.read()
         filtered_text = Util.clean_text(text)        
         return id, filtered_text
-    
-    def write_key(self, entries:int):
-        print("Awaiting Key Presentation...")
-        encrypted_entries = encrypt_message(str(entries))
-        self.reader.write(encrypted_entries)
