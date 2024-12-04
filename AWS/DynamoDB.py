da28569d7d749e25fb97ccd9df6085f9e9f0f26e
Liam Townsley <contact@liamtownsley.me>
2024-12-04 17:05:14 +0000

    feat: add AWS_SESSION_TOKEN

--- AWS/DynamoDB.py
+++ AWS/DynamoDB.py
@@ -5,7 +5,7 @@ from typing import Optional
 import os
 
 print(f"Access Key: {os.getenv('AWS_ACCESS_KEY_ID')}\nSecret Key: {os.getenv('AWS_SECRET_ACCESS_KEY')}\nRegion:{os.getenv('AWS_REGION')}")
-dynamodb = boto3.resource('dynamodb', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), region_name=os.getenv('AWS_REGION'))
+dynamodb = boto3.resource('dynamodb', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), aws_session_token=os.getenv('AWS_SESSION_TOKEN'), region_name=os.getenv('AWS_REGION'))
 users_table = dynamodb.Table('Users')
 access_log_table = dynamodb.Table('AccessLog')
 
--- AWS/S3.py
+++ AWS/S3.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 
-s3 = boto3.client('s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+s3 = boto3.client('s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), aws_session_token=os.getenv('AWS_SESSION_TOKEN'))
 
 def upload_to_s3(file_path, bucket_name, object_name):
     s3.upload_file(file_path, bucket_name, object_name)
