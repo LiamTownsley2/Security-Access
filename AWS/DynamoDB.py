3de3d4936b668170244fdc9b56ffc04c318511d3
Liam Townsley <contact@liamtownsley.me>
2024-12-04 16:52:58 +0000

    add debug log

--- AWS/DynamoDB.py
+++ AWS/DynamoDB.py
@@ -4,6 +4,7 @@ import random
 from typing import Optional
 import os
 
+print(f"Access Key: {os.getenv('AWS_ACCESS_KEY_ID')}\nSecret Key: {os.getenv('AWS_SECRET_ACCESS_KEY')}\nRegion:{os.getenv('AWS_REGION')}")
 dynamodb = boto3.resource('dynamodb', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), region_name=os.getenv('AWS_REGION'))
 users_table = dynamodb.Table('Users')
 access_log_table = dynamodb.Table('AccessLog')
