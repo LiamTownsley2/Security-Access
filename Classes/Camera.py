825b0c409bc92980feddb5a89c6ecae1ab55d9e6
Liam Townsley <contact@liamtownsley.me>
2024-12-05 00:47:03 +0000

    fix: camera

--- Classes/Camera.py
+++ Classes/Camera.py
@@ -5,9 +5,13 @@ import datetime
 picam = picamera.PiCamera()
 
 class Camera:
+    def __init__(self):
+        self.is_recording = False
+        
     def start_recording(self, seconds:int, user_id:str = None):       
         file_name = datetime.datetime.now(datetime.timezone.utc).isoformat() if user_id else user_id
         file_name = f"{file_name}.h264"
+        self.is_recording = True
         picam.start_recording(file_name)
         if seconds:
             sleep(seconds)
@@ -16,3 +20,4 @@ class Camera:
     
     def end_recording(self):
         picam.stop_recording()
+        self.is_recording = False
