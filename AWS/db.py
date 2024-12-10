4d0d53f4b6c0a87e37c3c5083f35cc6b3d4fc76b
Liam Townsley <contact@liamtownsley.me>
2024-12-10 19:54:06 +0000

    chore: remove unnecessary imports

--- AWS/db.py
+++ AWS/db.py
@@ -8,7 +8,7 @@ access_log_table = dynamodb.Table('AccessLog')
 
 thread_logger = logging.getLogger("ThreadLogger")
 
-from .DynamoDB.user import get_user, get_entries_count, delete_user, generate_unique_id, register_entry, register_user, get_all_users, edit_user
+from .DynamoDB.user import get_user, delete_user, generate_unique_id, register_user, get_all_users, edit_user
 from .DynamoDB.cards import get_user_by_card, register_card_to_user, remove_all_links_to_card
 from .DynamoDB.accessLog import register_entry, get_entries_count, get_all_logs
 
