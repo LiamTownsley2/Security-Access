fa16662214fede21c4f1433f4827ab7aa623db18
Liam Townsley <contact@liamtownsley.me>
2024-12-10 20:12:23 +0000

    debug: enable logging

--- API/index.py
+++ API/index.py
@@ -6,7 +6,7 @@ from .routes import accessLog, camera, card, user
 app = Flask(__name__)
 
 # disable flask logging
-logging.getLogger('werkzeug').disabled = True
+# logging.getLogger('werkzeug').disabled = True
 
 app.register_blueprint(accessLog.bp_access_log)
 app.register_blueprint(camera.bp_cameras)
