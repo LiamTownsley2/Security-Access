b1bb68fa295b4f7da5ac57f1260b8b1090477bb8
Liam Townsley <contact@liamtownsley.me>
2024-12-10 20:32:10 +0000

    fix: card read

--- API/routes/card.py
+++ API/routes/card.py
@@ -4,10 +4,9 @@ from AWS import db
 bp_cards = Blueprint('card', __name__, url_prefix='/card')
 
 @bp_cards.route('/<int:card_id>', methods=['GET'])
-def read_card():
-    data = request.get_json()
+def read_card(card_id):
     try:
-        user = db.get_user_by_card(data['CardID'])
+        user = db.get_user_by_card(str(card_id))
         if user is None:
             abort(404, description="This keycard is not linked to an Employee.")
         return jsonify(user), 200
