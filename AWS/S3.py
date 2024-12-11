d75ba2737749c6ce6cf4f2681490335e7e534c54
Liam Townsley <contact@liamtownsley.me>
2024-12-11 10:44:17 +0000

    refactor: add bucket to s3 upload_file

--- AWS/S3.py
+++ AWS/S3.py
@@ -4,7 +4,7 @@ import os
 s3 = boto3.client('s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), aws_session_token=os.getenv('AWS_SESSION_TOKEN'))
 
 def upload_to_s3(file_name, object_name):
-    s3.upload_file(file_name, object_name)
+    s3.upload_file(file_name, os.getenv(os.getenv("S3_BUCKET_NAME")), object_name)
     return os.getenv("S3_BUCKET_NAME"), object_name
 
 def get_videos_by_folder(directory_name):
