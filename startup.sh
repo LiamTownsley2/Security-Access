01dc8bb2bf05b483de11f94cd36d38781b7adf83
Liam Townsley <contact@liamtownsley.me>
2024-12-18 09:55:18 +0000

    fix: python directory referencing

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Setting up Linux Kernel Module..."
-./kernel/load_module.sh
+cd kernel/
+sudo ./load_module.sh
 echo -e "SUCCESS: Loaded Linux Kernel Module"
+cd ..
 
 echo "Setting up Python Application..."
-./app/load_python.sh
+cd app
+./load_python.sh
