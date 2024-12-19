af04e5a593e97c24f83c40a94f8417588786df92
Liam Townsley <contact@liamtownsley.me>
2024-12-19 19:53:43 +0000

    fix: imports

--- app/aws/db.py
+++ app/aws/db.py
@@ -1,6 +1,3 @@
-import boto3
-import os
-
 from .dynamoDB.user import (
     get_user,
     delete_user,
@@ -16,16 +13,7 @@ from .dynamoDB.cards import (
 )
 from .dynamoDB.accessLog import register_entry, get_entries_count, get_all_logs
 
-dynamodb = boto3.resource(
-    "dynamodb",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
-    region_name=os.getenv("AWS_REGION"),
-)
-
 __all__ = [
-    "dynamodb",
     # user
     "get_user",
     "delete_user",
