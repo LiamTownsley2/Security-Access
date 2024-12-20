b65c1a68fb8ada055b00853fb2d0295d83b48db2
Liam Townsley <contact@liamtownsley.me>
2024-12-20 15:33:45 +0000

    feat (dashboard): implement users page

--- app/api/routes/dashboard.py
+++ app/api/routes/dashboard.py
@@ -5,9 +5,11 @@ import os
 template_folder = os.path.join(os.path.dirname(__file__), '..', '..', 'templates')
 bp_dashboard = Blueprint("dashboard", __name__, url_prefix="/dashboard", template_folder=template_folder)
 
-users = []
 @bp_dashboard.route("/", methods=["GET"])
 def dashboard_index():
-    global users
+    return render_template("dashboard.html")
+
+@bp_dashboard.route("/", methods=["GET"])
+def dashboard_users():
     users = db.get_all_users()
-    return render_template("dashboard.html", users=users)
+    return render_template("users.html", users=users)
