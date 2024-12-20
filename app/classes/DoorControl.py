064f3561c50cbe0f4d119bd24b7dd368d48d0114
Liam Townsley <contact@liamtownsley.me>
2024-12-20 12:06:34 +0000

    x

--- app/classes/DoorControl.py
+++ app/classes/DoorControl.py
@@ -1,6 +1,10 @@
 from time import sleep
 from os import path
 from enum import Enum
+import logging
+import subprocess
+
+thread_logger = logging.getLogger("ThreadLogger")
 
 sysfs_path = "/sys/kernel/led_toggle/led_toggle"
 
@@ -19,9 +23,11 @@ class DoorController:
         return int(state)
 
     def lock(self):
+        thread_logger.info("LOCKING DOOR")
         self._write_state(DoorState.LOCKED)
 
     def unlock(self, seconds: int):
+        thread_logger.info("UNLOCKING DOOR")
         self._write_state(DoorState.UNLOCKED)
         if seconds:
             sleep(seconds)
@@ -33,6 +39,11 @@ class DoorController:
         else:
             self.lock()
 
-    def _write_state(self, state:int):
-        with open(sysfs_path, "w") as file:
-            file.write(str(state))
+    def _write_state(self, state: int):
+        thread_logger.info(f"Attempting to write state {str(state)}")
+
+        try:
+            subprocess.run(['sudo', 'sh', '-c', f'echo {state} > {sysfs_path}'], check=True)
+            thread_logger.info(f"Successfully wrote state {str(state)} to {sysfs_path}")
+        except subprocess.CalledProcessError as e:
+            thread_logger.error(f"Failed to write state to sysfs: {e}")
